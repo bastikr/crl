4bc1a05dbdc8f42145715c893cb8a40f0918b4f2
Sebastian Krämer <basti.kr@gmail.com>
2024-09-22 11:42:11 +0200

    Create directory for generated files in add_resource_library cmake function

--- lib/crl/cmake/resource_library.cmake
+++ lib/crl/cmake/resource_library.cmake
@@ -8,15 +8,18 @@ function(add_resource_library TARGET_NAME SOURCE_DIR)
     # Collect all files to rebuild when something changes
     file(GLOB_RECURSE RESOURCE_FILES CONFIGURE_DEPENDS "${SOURCE_DIR}/*")
 
-    set(output_cpp_file "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/src/${TARGET_NAME}.cpp")
-    set(output_h_file "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/include/${TARGET_NAME}.h")
+    set(target_directory "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}")
+    set(output_cpp_file "${target_directory}/src/${TARGET_NAME}.cpp")
+    set(output_h_file "${target_directory}/include/${TARGET_NAME}.h")
+
+    file(MAKE_DIRECTORY "${target_directory}")
     add_custom_command(
         OUTPUT "${output_cpp_file}" "${output_h_file}"
         COMMAND ${CMAKE_COMMAND} -D TARGET_NAME=${TARGET_NAME}
                                  -D SOURCE_DIR=${SOURCE_DIR}
                                  -P "${SRM_CMAKE_DIR}/generate.cmake"
         DEPENDS ${RESOURCE_FILES} "${SRM_CMAKE_DIR}/generate.cmake"
-        WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}"
+        WORKING_DIRECTORY "${target_directory}"
         COMMENT "Creating resource library for ${TARGET_NAME} from  ${SOURCE_DIR}"
         VERBATIM
     )
@@ -24,6 +27,6 @@ function(add_resource_library TARGET_NAME SOURCE_DIR)
 
     add_library(${TARGET_NAME} STATIC "${output_cpp_file}")
     add_dependencies(${TARGET_NAME} ${TARGET_NAME}_gen_target)
-    target_include_directories(${TARGET_NAME} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/include")
+    target_include_directories(${TARGET_NAME} PUBLIC "${target_directory}/include")
     target_link_libraries(${TARGET_NAME} PUBLIC crl)
 endfunction()
