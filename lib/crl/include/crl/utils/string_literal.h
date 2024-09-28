4852257f7369f1b9b3863b77d51ca193d5806043
Sebastian Krämer <sebastian.kraemer@securecell.ch>
2024-09-28 14:16:28 +0200

    Don't use algorithm.h in string_literal.h

--- lib/crl/include/crl/utils/string_literal.h
+++ lib/crl/include/crl/utils/string_literal.h
@@ -1,16 +1,16 @@
 #pragma once
 
-// TODO: Remove algorithm include
-#include <algorithm>
 #include <array>
 
 namespace crl {
 
-template <size_t N> struct StringLiteral {
+template <std::size_t N> struct StringLiteral {
     // NOLINTNEXTLINE(*-avoid-c-arrays)
     consteval StringLiteral(const char (&x)[N]) {
         static_assert(N > 0);
-        std::copy_n(static_cast<const char *>(x), N, value.data());
+        for (std::size_t i = 0; i < N; ++i) {
+            value[i] = x[i];
+        }
     }
 
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
