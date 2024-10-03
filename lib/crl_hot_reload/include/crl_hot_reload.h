0bd7dbbfa3530ff5ed94a6938f68af833cb5ff28
Sebastian Krämer <basti.kr@gmail.com>
2024-10-03 19:36:57 +0200

    Use std::filesystem::path as HotReloader base directory

--- lib/crl_hot_reload/include/crl_hot_reload.h
+++ lib/crl_hot_reload/include/crl_hot_reload.h
@@ -4,19 +4,18 @@
 #include <map>
 #include <optional>
 #include <span>
-#include <string_view>
 #include <vector>
 
 namespace crl {
 
 class HotReloader {
 public:
-    HotReloader(std::string_view base_dir);
+    HotReloader(std::filesystem::path base_dir);
 
     std::optional<std::span<std::byte>> get(std::string_view relative_path);
 
 private:
-    std::string_view m_base_dir;
+    std::filesystem::path m_base_dir;
 
     std::map<std::filesystem::path, std::vector<std::byte>> m_data;
 };
--- lib/crl_hot_reload/src/crl_hot_reload.cpp
+++ lib/crl_hot_reload/src/crl_hot_reload.cpp
@@ -2,17 +2,17 @@
 
 #include <filesystem>
 #include <fstream>
-
-namespace fs = std::filesystem;
+#include <utility>
 
 namespace crl {
 
-HotReloader::HotReloader(std::string_view base_dir) : m_base_dir(base_dir) {}
+HotReloader::HotReloader(std::filesystem::path base_dir)
+    : m_base_dir(base_dir) {}
 
 std::optional<std::span<std::byte>>
 HotReloader::get(std::string_view relative_path) {
-    auto path = fs::path(m_base_dir) / relative_path;
-    if (fs::is_regular_file(path)) {
+    auto path = m_base_dir / relative_path;
+    if (std::filesystem::is_regular_file(path)) {
         std::ifstream file(path, std::ios::binary);
         if (file.is_open()) {
             file.seekg(0, std::ios::end);
